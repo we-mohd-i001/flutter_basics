29120349249d758191db078c8d7645cf44ce50d5
Mohd Imran <mohd-i001@webreinvent.com>
2024-02-15 14:52:49 +0530

    added some basic widgets to the home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,101 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Basics'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Text('Some text!'),
+                Container(
+                  height: 60,
+                  width: 60,
+                  color: Colors.blue,
+                ),
+                Container(
+                  height: 60,
+                  width: 60,
+                  color: Colors.amber,
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(padding: const EdgeInsets.all(20),
+            child: Container(
+              height: 56,
+              width: 200,
+              color: Colors.lightBlue,
+            ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Center(
+              child: Text('Center Text!', style: TextStyle(
+                fontSize: 36
+              ),),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const SizedBox(
+              height: 200,
+              width: 200,
+              child: Placeholder(
+                color: Colors.brown,
+                strokeWidth: 5,
+                child: Center(child: Text('My PlaceHolder')),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const SizedBox(
+              height: 200,
+              width: 200,
+              child: Placeholder(
+                color: Colors.blue,
+                strokeWidth: 5,
+                child: Center(child: Text('My PlaceHolder')),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const SizedBox(
+              height: 200,
+              width: 200,
+              child: Placeholder(
+                color: Colors.red,
+                strokeWidth: 5,
+                child: Center(child: Text('My PlaceHolder')),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const SizedBox(
+              height: 200,
+              width: 200,
+              child: Placeholder(
+                color: Colors.green,
+                strokeWidth: 10,
+                child: Center(child: Text('My PlaceHolder')),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
